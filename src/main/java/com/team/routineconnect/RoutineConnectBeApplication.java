33b9c160830415521bb89c819da776b33109a30e
sunghyun1999 <32184099@dankook.ac.kr>
2023-08-15 18:58:26 +0900

    프로젝트 생성

---
+++ src/main/java/com/team/routineconnect/RoutineConnectBeApplication.java
@@ -0,0 +1,13 @@
+package com.team.routineconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutineConnectBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutineConnectBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team/routineconnect/RoutineConnectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.routineconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutineConnectBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
