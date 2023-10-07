e7ff6e3691402873b04e50f36f4dbd6bca7d7d5e
Hwang In Tae <sjobs66@gmail.com>
2023-10-07 21:06:00 +0900

    Import문 수정

--- src/main/java/com/team/routineconnect/config/OpenEntityManagerConfig.java
+++ src/main/java/com/team/routineconnect/config/OpenEntityManagerConfig.java
@@ -1,12 +1,9 @@
 package com.team.routineconnect.config;
 
-import io.swagger.models.auth.In;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
-
-import javax.servlet.FilterRegistration;
 
 @Configuration
 public class OpenEntityManagerConfig {
