0929bb3c7381c208655755ed66d608165f813262
Hwang In Tae <sjobs66@gmail.com>
2024-03-29 14:42:23 +0900

    feat(project): spring-boot-starter-validation 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,76 +1,77 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.2.3"
-	id("io.spring.dependency-management") version "1.1.4"
-	id("org.asciidoctor.jvm.convert") version "3.3.2"
+    java
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
 }
 
 group = "kr.online"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 val snippetsDir by extra { file("build/generated-snippets") }
 val asciidoctorExt: Configuration by configurations.creating
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.security:spring-security-test")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("io.jsonwebtoken:jjwt-api:0.12.3")
-	compileOnly("org.projectlombok:lombok")
-	runtimeOnly("com.h2database:h2")
-	runtimeOnly("com.mysql:mysql-connector-j")
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
-	asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.security:spring-security-test")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-	jvmArgs("-Dfile.encoding=UTF-8")
-	outputs.dir(snippetsDir)
+    useJUnitPlatform()
+    jvmArgs("-Dfile.encoding=UTF-8")
+    outputs.dir(snippetsDir)
 }
 
 tasks.asciidoctor {
-	configurations(asciidoctorExt.name)
-	dependsOn(tasks.test)
-	doFirst {
-		delete(file("src/main/resources/static/docs"))
-	}
-	inputs.dir(snippetsDir)
-	doLast {
-		copy {
-			from("build/docs/asciidoc")
-			into("src/main/resources/static/docs")
-		}
-	}
+    configurations(asciidoctorExt.name)
+    dependsOn(tasks.test)
+    doFirst {
+        delete(file("src/main/resources/static/docs"))
+    }
+    inputs.dir(snippetsDir)
+    doLast {
+        copy {
+            from("build/docs/asciidoc")
+            into("src/main/resources/static/docs")
+        }
+    }
 }
 
 sourceSets {
-	main {
-		java.srcDirs("src/main/java")
-		resources.srcDirs("src/main/resources/static/docs")
-	}
+    main {
+        java.srcDirs("src/main/java")
+        resources.srcDirs("src/main/resources/static/docs")
+    }
 }
 
 tasks.getByName<Jar>("jar") {
-	enabled = false
+    enabled = false
 }
