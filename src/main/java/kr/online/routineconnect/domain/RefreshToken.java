4a8a8c4159f697b55287a29852b7d5c98333a9fc
Hwang In Tae <sjobs66@gmail.com>
2024-03-29 11:13:01 +0900

    refactor(jwt): protected 기본 생성자 추가

--- src/main/java/kr/online/routineconnect/domain/RefreshToken.java
+++ src/main/java/kr/online/routineconnect/domain/RefreshToken.java
@@ -1,12 +1,15 @@
 package kr.online.routineconnect.domain;
 
 import java.util.concurrent.TimeUnit;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @RedisHash("refreshToken")
 public class RefreshToken {
 
