4d4d5e13fc1ef71ba0b3c594201945af13395fe5
Hwang In Tae <sjobs66@gmail.com>
2024-03-29 11:47:31 +0900

    refactor(auth): @Repository 추가

--- src/main/java/kr/online/routineconnect/repository/UserRepository.java
+++ src/main/java/kr/online/routineconnect/repository/UserRepository.java
@@ -3,7 +3,9 @@ package kr.online.routineconnect.repository;
 import java.util.Optional;
 import kr.online.routineconnect.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
