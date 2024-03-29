2d44cb4713ab0721688a9d769befa6ee50a20335
Hwang In Tae <sjobs66@gmail.com>
2024-03-29 14:40:47 +0900

    feat(auth): 이름 이메일 중복체크 JPA 메서드 추가

--- src/main/java/kr/online/routineconnect/repository/UserRepository.java
+++ src/main/java/kr/online/routineconnect/repository/UserRepository.java
@@ -9,4 +9,8 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByName(String name);
 }
