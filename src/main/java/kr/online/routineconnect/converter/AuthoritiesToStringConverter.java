5211206b91c04736b8760395a277829434a4fa80
Hwang In Tae <sjobs66@gmail.com>
2024-04-02 21:20:43 +0900

    feat(auth): Authority⇔String 변환하는 컨버터 생성

---
+++ src/main/java/kr/online/routineconnect/converter/AuthoritiesToStringConverter.java
@@ -0,0 +1,26 @@
+package kr.online.routineconnect.converter;
+
+import jakarta.persistence.AttributeConverter;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public class AuthoritiesToStringConverter implements
+        AttributeConverter<Collection<? extends GrantedAuthority>, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Collection<? extends GrantedAuthority> attribute) {
+        return attribute.stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> convertToEntityAttribute(String dbData) {
+        return Arrays.stream(dbData.split(","))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+}
