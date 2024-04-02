de779642bc10415481b13fb53af67e87d1d821c7
Hwang In Tae <sjobs66@gmail.com>
2024-04-02 21:24:04 +0900

    feat(project): JpaAuditing과 redis 활성화

--- src/main/java/kr/online/routineconnect/RoutineConnectApplication.java
+++ src/main/java/kr/online/routineconnect/RoutineConnectApplication.java
@@ -2,12 +2,17 @@ package kr.online.routineconnect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
+@EnableJpaAuditing
+@EnableRedisRepositories
 @SpringBootApplication
 public class RoutineConnectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RoutineConnectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RoutineConnectApplication.class, args);
+    }
 
 }
+
