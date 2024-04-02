3c2bec5e77ab7aa43ec379f31f929c60bd879263
Hwang In Tae <sjobs66@gmail.com>
2024-04-03 00:33:28 +0900

    refactor(auth): 컨버터에 @Converter @Component 추가
    @Convert를 추가해 컨버터임을 명시하고 TokenProvider에서 사용되므로 @Component를 추가해 빈으로 등록함.

--- src/main/java/kr/online/routineconnect/converter/AuthoritiesToStringConverter.java
+++ src/main/java/kr/online/routineconnect/converter/AuthoritiesToStringConverter.java
@@ -1,12 +1,16 @@
 package kr.online.routineconnect.converter;
 
 import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Collectors;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
 
+@Component
+@Converter
 public class AuthoritiesToStringConverter implements
         AttributeConverter<Collection<? extends GrantedAuthority>, String> {
 
