25e7fbafde212b8f4718118877ad29e6167a53a6
Hwang In Tae <sjobs66@gmail.com>
2024-04-04 20:30:53 +0900

    test(auth): 인증 관련 API 명세를 위한 테스트 추가
    Rest Docs를 이용해 API 명세를 위한 테스트를 추가하고 이를 위해 기존 코드를 수정함.
    
    주요 변경 사항:
    - 스니펫의 요청 주소를 백엔드 서버 주소로 설정하는 등 AsciiDoc 코드 스니펫 설정을 위해 ApiDocumentUtils 생성
    - Rest Docs 스니펫 생성을 위한 테스트 코드 작성
    - @WebMvcTest를 이용해 컨트롤러 계층 단위 테스트하기 위해 @EnableJpaAuditing, @EnableRedisRepositories RoutineConnectApplicatioin 에서 Config파일로 이동
    - 테스트에서 인스턴스 생성을 위해 RefreshAccessTokenRequest, SignUpRequest, SignInRequest DTO에 @Builder 추가

--- src/main/java/kr/online/routineconnect/dto/SignUpRequest.java
+++ src/main/java/kr/online/routineconnect/dto/SignUpRequest.java
@@ -1,6 +1,8 @@
 package kr.online.routineconnect.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
+@Builder
 public record SignUpRequest(@NotBlank String email, @NotBlank String name, @NotBlank String password) {
 }
---
+++ src/test/java/kr/online/routineconnect/ApiDocumentUtils.java
@@ -0,0 +1,24 @@
+package kr.online.routineconnect;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+
+public interface ApiDocumentUtils {
+    static OperationRequestPreprocessor getDocumentRequest() {
+        return preprocessRequest(
+                modifyUris()
+                        .scheme("https")
+                        .host("xn--b60b50mo0tbqdri.xn--oi2b61z32a.xn--3e0b707e")
+                        .removePort(),
+                prettyPrint());
+    }
+
+    static OperationResponsePreprocessor getDocumentResponse() {
+        return preprocessResponse(prettyPrint());
+    }
+}
